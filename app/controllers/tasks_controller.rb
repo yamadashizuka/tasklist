d5d8b5a49e616b07dc10e59f79c513b317931274
yamadashizuka <P0372979@P0372979B.ad.ogis-ri.co.jp>
2014-11-12 14:16:08 +0900

    更にとりあえず

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,6 +28,7 @@ class TasksController < ApplicationController
 
     respond_to do |format|
       if @task.save
+        @task.dump
         format.html { redirect_to @task, notice: 'Task was successfully created.' }
         format.json { render :show, status: :created, location: @task }
       else
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,15 @@
 class Task < ActiveRecord::Base
   belongs_to :status
+
+  def dump
+    puts "#################### dump Start!"
+    puts Task.attribute_names
+    puts "-------------"
+    puts "-------------"
+    Task.columns.each{|col|
+      puts col.name
+      puts self.column_for_attribute(col.name)
+      }
+    puts "#################### dump End!"
+  end
 end
